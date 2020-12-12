91505a7ca98b7eb14e322830153418f910ebd596
NikitaKr <nikita.fyvfyv@gmail.com>
2020-12-12 17:29:48 +0200

    Delete useless templates

--- beer/views.py
+++ beer/views.py
@@ -8,14 +8,20 @@ from .forms import AuthUserForm, CreateUserForm
 
 
 class LoginUserView(LoginView):
+    """View for authentication users"""
+
     template_name = 'account/login.html'
     form_class = AuthUserForm
     success_url = reverse_lazy('#')
 
 
 class RegisterUserView(CreateView):
+    """View for register users"""
+
     model = User
     template_name = 'account/register.html'
     form_class = CreateUserForm
     success_url = reverse_lazy('#')
+
+
 
